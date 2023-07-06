733af32fa29c3bc6142f60be552529929a192272
santoshkumarm1 <santoshkumarm1@verifone.com>
2023-07-06 17:21:52 +0530

    initiql commit.

---
+++ order-service-tdd-api/src/test/java/com/demo/multi/orderservicetddapi/OrderServiceTddApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.multi.orderservicetddapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceTddApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
