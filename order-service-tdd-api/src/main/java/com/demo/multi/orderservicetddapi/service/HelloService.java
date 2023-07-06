50b84d76ba5f2b0e701c35b8dcb84b580d6c6e27
santoshkumarm1 <santoshkumarm1@verifone.com>
2023-07-06 18:24:03 +0530

    test java

--- order-service-tdd-api/src/main/java/com/demo/multi/orderservicetddapi/service/HelloService.java
+++ order-service-tdd-api/src/main/java/com/demo/multi/orderservicetddapi/service/HelloService.java
@@ -8,4 +8,12 @@ public class HelloService {
     public String getProjectDetails(String projectName) {
         return "DB:" + projectName;
     }
+
+    public int sumService(int n1, int n2) {
+        return n1 + n2;
+    }
+
+    public int diffService(int n1, int n2) {
+        return n1 - n2;
+    }
 }
