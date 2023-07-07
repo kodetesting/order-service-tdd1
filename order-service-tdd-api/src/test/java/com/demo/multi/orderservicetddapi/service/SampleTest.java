90faa4abb5e87aea3ad96fc470134af94446130e
santoshkumarm1 <santoshkumarm1@verifone.com>
2023-07-07 19:39:09 +0530

    test java

---
+++ order-service-tdd-api/src/test/java/com/demo/multi/orderservicetddapi/service/SampleTest.java
@@ -0,0 +1,15 @@
+package com.demo.multi.orderservicetddapi.service;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class SampleTest {
+
+    @Test
+    void test1() {
+        Assertions.fail("this is failed");
+    }
+}
